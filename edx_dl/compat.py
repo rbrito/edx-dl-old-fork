2a21794824c4d8fd814e4e9fccb0e5db5936398d
Rogério Brito <rbrito@ime.usp.br>
2014-01-06 23:08:14 -0200

    edx_dl: compat: Reformat imports, now that we know that it works.
    The code was written that way to check for problems with each import (that
    is, to get line numbers in each of the potentially failed import), but the
    code works OK.

--- edx_dl/compat.py
+++ edx_dl/compat.py
@@ -13,15 +13,9 @@ except ImportError:
 # urllib, urllib2
 try:
     from urllib import urlencode
-    from urllib2 import HTTPCookieProcessor
-    from urllib2 import Request
-    from urllib2 import urlopen
-    from urllib2 import build_opener
-    from urllib2 import install_opener
+    from urllib2 import (HTTPCookieProcessor, Request, urlopen,
+                         build_opener, install_opener)
 except ImportError:
     from urllib.parse import urlencode
-    from urllib.request import HTTPCookieProcessor
-    from urllib.request import Request
-    from urllib.request import urlopen
-    from urllib.request import build_opener
-    from urllib.request import install_opener
+    from urllib.request import (HTTPCookieProcessor, Request, urlopen,
+                                build_opener, install_opener)
